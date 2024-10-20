3759a53f506f183c06b6242b3e300cc712d852be
Marce <marcelobaltazarpalma@gmail.com>
2024-10-20 16:43:24 -0300

    start the main component

---
+++ app/components/main/main.tsx
@@ -0,0 +1,23 @@
+export default function Main() {
+    return (
+        <section className='py-24'>
+            <div className="mx-auto px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                <header className="text-center ">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-4xl lg:leading-normal font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-pink-600 z-10">
+                            Lulu Gumis
+                        </span>
+                    </h1>
+                    <p className="mx-auto mt-4 max-w-md text-black font-semibold">
+                        Luli 🌷| Amigurumis, flores eternas, tejidos a crochet.
+                        Shopping & retail
+                        《ꕤ》➫ [ Agenda abierta ✨]༄
+                        📍Concepción, chile.
+                        Tejidos personalizados hecho a mano con mucho lov♡.
+                    </p>
+                </header>
+            </div>
+        </section>
+    )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import Navbar from "./components/navigation/Navbar";
+import Main from "./components/main/main";
 
 export default function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
       <header>
 
         <Navbar />
-        
+
       </header>
+      <main>
+        <Main />
+      </main>
     </div>
   );
 }

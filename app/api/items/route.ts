335cbaa9c78931639cf7baf7f038c77a33946879
Marce <marcelobaltazarpalma@gmail.com>
2024-10-24 23:40:20 -0300

    a

--- app/api/items/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request: Request) {
-    // Logica para despues
-    return NextResponse.json({ message: "Hello from the items API!" });
-}

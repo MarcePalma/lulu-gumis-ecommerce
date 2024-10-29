687b846756b1235cd4a9c238f92375b06944fb9b
Marce <marcelobaltazarpalma@gmail.com>
2024-10-29 15:18:28 -0300

    Implemented Suspense around Catalogo component to handle useSearchParams. Improved category selection handling in SubNavbar

--- app/components/catalogo/catalogo.tsx
+++ app/components/catalogo/catalogo.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import PostCards from './post';
 import SubNavbar from './subnavbar';
@@ -15,19 +15,21 @@ export default function Catalogo() {
     }, [searchParams]);
 
     return (
-        <div>
-            <div className="py-20 mt-10">
-                <SubNavbar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-            </div>
+        <Suspense fallback={<div>Cargando...</div>}>
+            <div>
+                <div className="py-20 mt-10">
+                    <SubNavbar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+                </div>
 
-            <div className="text-center">
-                <h1 className="sparkling-title text-white mb-2 text-4xl sm:text-4xl lg:text-4xl lg:leading-normal font-extrabold">
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-pink-600 z-10">
-                        Catalogo
-                    </span>
-                </h1>
-                <PostCards category={selectedCategory} />
+                <div className="text-center">
+                    <h1 className="sparkling-title text-white mb-2 text-4xl sm:text-4xl lg:text-4xl lg:leading-normal font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-pink-600 z-10">
+                            Catalogo
+                        </span>
+                    </h1>
+                    <PostCards category={selectedCategory} />
+                </div>
             </div>
-        </div>
+        </Suspense>
     );
 }
